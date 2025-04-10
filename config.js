45de00256fe731c861bfb864acd2b5dabaa97dc2
srima <dsrimathi2021@gmail.com>
2025-04-10 17:04:39 +0530

    l

---
+++ config.js
@@ -0,0 +1,3 @@
+export const PORT = process.env.PORT || 8000;
+
+export const mongoURL = "mongodb+srv://srimathi2004:Srima%402004@findit-db.wy0s91s.mongodb.net/lost-items?retryWrites=true&w=majority";
